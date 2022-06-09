58b100063fedd0b0d9cf6a9771b835ea0401d890
litao@tuya.com <litao@tuya.com>
2022-06-09 16:22:21 +0800

    增加 xmltodict 模块自动安装

--- components/my_depend/my_depend.py
+++ components/my_depend/my_depend.py
@@ -22,6 +22,11 @@ class my_depend():
             self.__check_and_install('kconfiglib','kconfiglib')           
 
         try:
+            import xmltodict
+        except ImportError as e:
+            self.__check_and_install('xmltodict','xmltodict')
+            
+        try:
             import lxml
         except ImportError as e:
             self.__check_and_install('lxml','lxml')
